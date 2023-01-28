fc0b2c5201aa25821a00139a049032e05d985d2a
Igor Ming <igor.ming@gmail.com>
2023-01-28 12:02:29 -0300

    flutterfire configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:muda_facil/firebase_options.dart';
 import 'package:muda_facil/src/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
