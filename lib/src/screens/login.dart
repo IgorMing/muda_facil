5bc0fb1b40f3bc5a49fae3c073c3f7abfd8cf1f5
Igor Ming <igor.ming@gmail.com>
2023-01-28 13:00:42 -0300

    create basic login screen's UI

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -1,14 +1,67 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('This is login screen'),
-      ),
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 80,
+          ),
+          const TextField(
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(
+              labelText: 'Email',
+            ),
+          ),
+          const TextField(
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+              labelText: 'Password',
+            ),
+            obscureText: true,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton.icon(
+            onPressed: signIn,
+            label: const Text('Sign in'),
+            icon: const Icon(
+              Icons.lock_open,
+            ),
+          )
+        ],
+      )),
+    );
+  }
+
+  Future signIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
     );
   }
 }
