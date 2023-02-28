4548b1f9ca1b347146fb8f3246c647039b19adc3
Igor Ming <igor.ming@gmail.com>
2023-02-28 18:26:44 -0300

    remove slidable refs

--- lib/src/widgets/item_tile.dart
+++ lib/src/widgets/item_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:muda_facil/src/models/item.dart';
 import 'package:muda_facil/src/utils/ui.dart';
 import 'package:muda_facil/src/widgets/item_counter.dart';
@@ -22,12 +21,28 @@ class ItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Slidable(
-      endActionPane: ActionPane(
-        motion: const ScrollMotion(),
+    return Card(
+      elevation: 4.0,
+      child: ExpansionTile(
+        key: GlobalKey(),
+        // contentPadding: const EdgeInsets.all(4.0),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("${data.name} ( ${data.amount} )"),
+            ItemCounter(
+              onMinus: onMinus,
+              onPlus: onPlus,
+            )
+          ],
+        ),
+        subtitle: data.comment != null && data.comment!.isNotEmpty
+            ? Text(data.comment ?? '')
+            : null,
         children: [
-          SlidableAction(
-            onPressed: (context) {
+          ListTile(
+            title: const Text('Adicionar observação'),
+            onTap: () {
               UIUtils.showInputDialog(
                 context,
                 onSave: (text) {
@@ -36,61 +51,23 @@ class ItemTile extends StatelessWidget {
                 initialText: data.comment,
               );
             },
-            icon: Icons.details,
-            label: "Observação",
-            backgroundColor: Colors.yellow.shade800,
-            foregroundColor: Colors.white,
           ),
-        ],
-      ),
-      child: Card(
-        elevation: 4.0,
-        child: ExpansionTile(
-          key: GlobalKey(),
-          // contentPadding: const EdgeInsets.all(4.0),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("${data.name} ( ${data.amount} )"),
-              ItemCounter(
-                onMinus: onMinus,
-                onPlus: onPlus,
-              )
-            ],
-          ),
-          subtitle: data.comment != null && data.comment!.isNotEmpty
-              ? Text(data.comment ?? '')
-              : null,
-          children: [
-            ListTile(
-              title: const Text('Adicionar observação'),
-              onTap: () {
-                UIUtils.showInputDialog(
-                  context,
-                  onSave: (text) {
-                    onAddComment!(text);
-                  },
-                  initialText: data.comment,
-                );
-              },
-            ),
-            ListTile(
-              onTap: () {
-                UIUtils.showAlertDialog(context, onSelect: (selected) {
-                  if (selected) {
-                    onRemove!();
-                  }
-                });
-              },
-              title: Text(
-                'Remover item',
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.error,
-                ),
+          ListTile(
+            onTap: () {
+              UIUtils.showAlertDialog(context, onSelect: (selected) {
+                if (selected) {
+                  onRemove!();
+                }
+              });
+            },
+            title: Text(
+              'Remover item',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.error,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
